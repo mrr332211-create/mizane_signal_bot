0d529d53bfe20ce68d4fadbe7641ed9c4a084f31
mrr332211-create <m.rr332211@gmail.com>
2025-08-15 15:53:47 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-BOT_TOKEN
-CHAT_ID
-GOLD_API_KEY
-USD_API_URL
-START_HOUR (10)
-END_HOUR (19)
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+GOLD_API_KEY = os.getenv("GOLD_API_KEY")
+USD_API_URL = os.getenv("USD_API_URL")
+START_HOUR = int(os.getenv("START_HOUR", 10))
+END_HOUR = int(os.getenv("END_HOUR", 19))
