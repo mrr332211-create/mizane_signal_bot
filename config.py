2da18cbe242ba0cb8fb23e51f8fb3b0a6ab9f03b
mrr332211-create <m.rr332211@gmail.com>
2025-08-15 15:50:53 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
-
-if not BOT_TOKEN or not CHAT_ID:
-    raise ValueError("BOT_TOKEN or CHAT_ID is not set in environment variables.")
-  
+BOT_TOKEN
+CHAT_ID
+GOLD_API_KEY
+USD_API_URL
+START_HOUR (10)
+END_HOUR (19)
